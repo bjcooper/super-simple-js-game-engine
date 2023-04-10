74c7e52d9a1a5d97ac273ce3221361088113021b
Brett Cooper <brett@thewiznerd.com>
2023-04-10 12:13:16 -0600

    Fixed types export.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,25 @@
+import { GameEngine } from './GameEngine';
 export { GameEngine } from './GameEngine';
 export { GameEntityBase } from './GameEntityBase';
 export { AgeTrait } from './composition/AgeTrait';
 export { PositionTrait } from './composition/PositionTrait';
 export { SizeTrait } from './composition/SizeTrait';
 export { StateTrait } from './composition/StateTrait';
+export type UpdateCallback = (deltaMs: number) => void;
+export type DrawCallback = (ctx: CanvasRenderingContext2D) => void;
+export interface GameEntity {
+    game: GameEngine;
+    remove: () => void;
+    update?: UpdateCallback;
+    draw?: DrawCallback;
+}
+export type Vector2D = {
+    x: number;
+    y: number;
+};
+export type BoundingBoxInfo = {
+    top: () => number;
+    bottom: () => number;
+    left: () => number;
+    right: () => number;
+};
